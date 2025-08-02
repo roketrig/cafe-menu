6e2ec8ee1b62a23e0e764a261f9ae4e18e138ab5
bugra <bugra.ksr@gmail.com>
2025-08-02 16:08:19 +0300

    update

--- menu.js
+++ menu.js
@@ -1,180 +1,196 @@
 const menuData = [
   {
     name: "Americano",
+    category: "kahveler",
     description: "Hafif içimli, sade kahve severler için ideal.",
     price: "130 TL",
     image: "https://cdn.shopify.com/s/files/1/2714/0802/files/americano-nasil-yapilir-tarif_480x480.jpg?v=1688990813"
   },
     {
     name: "Ice Americano",
+    category: "kahveler",
     description: "Hafif içimli, sade kahve severler için ideal.",
     price: "140 TL",
     image: "https://cdn.shopify.com/s/files/1/2714/0802/files/americano-nasil-yapilir-tarif_480x480.jpg?v=1688990813"
   },
   {
     name: "Espresso",
+    category: "kahveler",
     description: "Yoğun çikolatalı, fırınlanmış cheesecake lezzeti.",
     price: "80 TL",
     image: "https://www.uzaypastanesi.com/images/product/SEBASTIAN-7_26Gl.jpg"
   },
     {
     name: "Double Espresso",
+    category: "kahveler",
     description: "Yoğun çikolatalı, fırınlanmış cheesecake lezzeti.",
     price: "130 TL",
     image: "https://www.uzaypastanesi.com/images/product/SEBASTIAN-7_26Gl.jpg"
   },
   {
     name: "Filtre",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "130 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Ice Filtre",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "140 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Latte",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "150 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
       {
     name: "Ice Latte",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "160 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Flat White",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "145 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Cortado",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "140 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Cappicino",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "150 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Macchiato",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "130 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Karamel Macchiato",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "165 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Ice Karamel Macchiato",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "180 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Mocha",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "165 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
         {
     name: "Ice Choclate Mocha",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "180 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Türk Kahvesi",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "100 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Double Türk Kahvesi",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "150 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Sıcak Çikolata",
-    description: "Kaşar peynirli, domatesli klasik lezzet.",
-    price: "150 TL",
-    image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
-  },
-    {
-    name: "Sıcak Çikolata",
-    description: "Kaşar peynirli, domatesli klasik lezzet.",
-    price: "150 TL",
-    image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
-  },
-    {
-    name: "Sıcak Çikolata",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "150 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
   {
     name: "Çikolata ve Çilekli Milkshake",
+    category: "kahveler",
     description: "Kaşar peynirli, domatesli klasik lezzet.",
     price: "190 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
   {
     name: "Smoothie",
+    category: "kahveler",
     description: "Fıstık Ezmesi, Muz, Şeftali",
     price: "185 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Limonata",
+    category: "kahveler",
     description: "Fıstık Ezmesi, Muz, Şeftali",
     price: "170 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Ice Tea",
+    category: "kahveler",
     description: "Fıstık Ezmesi, Muz, Şeftali",
     price: "140 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Kola",
+    category: "kahveler",
     description: "Fıstık Ezmesi, Muz, Şeftali",
     price: "80 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
       {
     name: "Gazoz",
+    category: "kahveler",
     description: "Fıstık Ezmesi, Muz, Şeftali",
     price: "80 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Soda",
+    category: "kahveler",
     description: "Fıstık Ezmesi, Muz, Şeftali",
     price: "60 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Limonlu Soda",
+    category: "kahveler",
     description: "Fıstık Ezmesi, Muz, Şeftali",
     price: "70 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Churchill",
+    category: "kahveler",
     description: "Fıstık Ezmesi, Muz, Şeftali",
     price: "100 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
@@ -185,48 +201,56 @@ const menuData = [
   */
      {
     name: "Hindi Füme Sandviç",
+    category: "yemekler",
     description: "Hindi füme, Yeşillik, Kaşar, Mısır, Zeytin, Sos",
     price: "320 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
       {
     name: "Somon Sandviç",
+    category: "yemekler",
     description: "Somon, Yeşillik, Kaşar, Mısır, Zeytin, Sos",
     price: "380 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
       {
     name: "Üç Peynirli Sandviç",
+    category: "yemekler",
     description: "Cheddar, Kaşar, Çeçil Peyniri",
     price: "340 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
       {
     name: "Peynirli Tost",
+    category: "yemekler",
     description: "Beyaz Peynir, Fesleğen, Domates",
     price: "220 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
         {
     name: "Kaşarlı Tost",
+    category: "yemekler",
     description: "Bol Kaşarlı",
     price: "180 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
         {
     name: "Hindi Füme Tost",
+    category: "yemekler",
     description: "Hindi Füme, Kaşar",
     price: "210 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
   {
     name: "Hindi Füme",
+    category: "yemekler",
     description: "Hindi füme",
     price: "200 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Poğaçalar",
+    category: "yemekler",
     description: "",
     price: "60 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
@@ -237,6 +261,7 @@ const menuData = [
   */
    {
     name: "San Sebastian",
+    category: "tatlılar",
     description: "Dondurma + 60 TL",
     price: "270 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
@@ -244,19 +269,22 @@ const menuData = [
 
     {
     name: "Kekler",
-    description: "Hindi füme",
+    category: "tatlılar",
+    description: "",
     price: "160 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
       {
     name: "Dondurma",
+    category: "tatlılar",
     description: "Sadece Tatlılar ile servis edilir.",
     price: "60 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
     {
     name: "Günün Tatlısı",
-    description: "Hindi füme",
+    category: "tatlılar",
+    description: "",
     price: "180 TL",
     image: "https://static.ticimax.cloud/cdn-cgi/image/width=-,quality=85/9247/uploads/urunresimleri/buyuk/kasarli-tost-dcb9.jpg"
   },
@@ -264,22 +292,34 @@ const menuData = [
   
 ];
 
-const container = document.getElementById('menu-container');
+const container = document.getElementById("menu-container");
 
-menuData.forEach(item => {
-  const div = document.createElement('div');
-  div.className = 'menu-item';
-  div.innerHTML = `
-    <div class="item-image">
-      <img src="${item.image}" alt="${item.name}">
-    </div>
-    <div class="item-info">
-      <h3>${item.name}</h3>
-      <p>${item.description}</p>
-    </div>
-    <div class="item-price">
-      <span>${item.price}</span>
-    </div>
-  `;
-  container.appendChild(div);
-});
+function renderMenu(category) {
+  container.innerHTML = "";
+  const filtered = menuData.filter(item => item.category === category);
+
+  filtered.forEach(item => {
+    const div = document.createElement("div");
+    div.className = "menu-item";
+    div.innerHTML = `
+      <div class="item-image">
+        <img src="${item.image}" alt="${item.name}">
+      </div>
+      <div class="item-info">
+        <h3>${item.name}</h3>
+        <p>${item.description}</p>
+      </div>
+      <div class="item-price">
+        <span>${item.price}</span>
+      </div>
+    `;
+    container.appendChild(div);
+  });
+}
+
+document.getElementById("btn-kahveler").addEventListener("click", () => renderMenu("kahveler"));
+document.getElementById("btn-tatlılar").addEventListener("click", () => renderMenu("tatlılar"));
+document.getElementById("btn-yemekler").addEventListener("click", () => renderMenu("yemekler"));
+
+
+renderMenu("kahveler");
